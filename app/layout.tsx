0f4f6d905afeb7bb20106355d87c0a9393efc396
unknown <conventionalanimation@gmail.com>
2025-11-01 21:48:06 +0100

    corrected fb pixel installation

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,38 +26,40 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Facebook Pixel Script */}
+        {/*  Meta Pixel Code */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
-        <!-- Meta Pixel Code -->
+            !(function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            })(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
 
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', ${FB_PIXEL_ID});
-fbq('track', 'PageView');
-
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
-        `}
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
         </Script>
 
-        {/* Noscript fallback */}
+        {/*  Noscript OUTSIDE Script tag */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
 
         {children}
       </body>
